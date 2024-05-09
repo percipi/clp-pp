338a1d0b2479f8fcef6bf88d7d08965bcec40665
Lukasz Pater <lukaszjpater@gmail.com>
2024-05-09 06:08:21 +0200

    add tailwindcss-typography

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
     return (
-        <main>
+        <main className="prose">
             <h1>purchasing process</h1>
             <h2>cart</h2>
             <ul>
