7ed3fc7b7717d917f8082666555df4854f5c73cc
Lukasz Pater <lukaszjpater@gmail.com>
2024-05-09 06:17:38 +0200

    add xstate/react

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,30 @@
-import Image from 'next/image';
+'use client';
+
+import { createActor, createMachine } from 'xstate';
+
+const toggleMachine = createMachine({
+    id: 'toggle',
+    initial: 'cart',
+    states: {
+        cart: {
+            on: { address: 'adressed' },
+        },
+        adressed: {
+            on: { select_shipping: 'shipping_selected' },
+        },
+        shipping_selected: {},
+    },
+});
+
+const actor = createActor(toggleMachine);
+
+// Subscribe to snapshots (emitted state changes) from the actor
+actor.subscribe((snapshot) => {
+    console.log('Value:', snapshot.value);
+});
+
+// Start the actor
+actor.start(); // logs 'Inactive'
 
 export default function Home() {
     return (
@@ -10,7 +36,12 @@ export default function Home() {
                 <li>Product 2</li>
                 <li>Product 3</li>
             </ul>
-            <button className="btn">Go to address</button>
+            <button
+                className="btn"
+                onClick={() => actor.send({ type: 'address' })}
+            >
+                Go to address
+            </button>
         </main>
     );
 }
